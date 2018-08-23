f1380b14ce0ecf4296f4f566f8b75efe9ae97a1d
Ed Me <603803799@qq.com>
2018-08-24 07:27:53 +0800

    fix:修复大屏幕下，部分元素大小和预期不符的情况。

--- db/index.js
+++ db/index.js
@@ -19,6 +19,7 @@ const types = [] // article type
 const run = main => {
   articles.length = 0 // clear
   types.length = 0 // clear
+
   types.push(...getDirectorys(resolve(CATALOG_ONE)).// update article type
   map(directory => directory.split('/').slice(-1)[0]))
 
@@ -34,8 +35,7 @@ const run = main => {
       const files = getFiles(resolve(CATALOG_ONE, type, article))
       const mdFile = files.find(file => file.includes(ARTICLE_SUFFIX))
       const coverFile = files.find(file => file.includes(COVER_SUFFIX))
-      console.log(strToTimeStamp(
-        mdFile.split('/').slice(-1)[0].replace(ARTICLE_SUFFIX, '')))
+
       articles.push({ // update article list
         type,
         title: article.split('/').slice(-1)[0],
