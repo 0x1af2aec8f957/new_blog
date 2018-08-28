c5f1e1f8069e4be1f516e16b804f510af9f7a8ae
Ed Me <603803799@qq.com>
2018-08-28 17:00:59 +0800

    fix:修改邮件服务器为QQ邮箱

--- config.js
+++ config.js
@@ -3,6 +3,12 @@ const SUBSCRIBE_USERS = [ // Subscribe user email
   '"Sweet° 尛亼物"<1072682534@qq.com>',
 ]
 
+const EmailAuth = {
+  user: '603803799@qq.com',
+  pass: 'xurzskuzbtwxbfdf',
+}
+
 module.exports = {
   SUBSCRIBE_USERS,
+  EmailAuth,
 }
--- server/sendEmail.js
+++ server/sendEmail.js
@@ -1,18 +1,16 @@
 const nodemailer = require('nodemailer')
-const {SUBSCRIBE_USERS} = require('../config') // Subscribe user email
+const {SUBSCRIBE_USERS, EmailAuth} = require('../config') // Subscribe user email
 
 const transporter = nodemailer.createTransport({
-  service: 'hotmail',
-  // port: 587,
+  // service: 'hotmail',
+  service: 'qq',
+  port: 465,
   secure: false, // true for 465, false for other ports
-  auth: {
-    user: 'quanchuan@live.cn', // generated ethereal user
-    pass: 'www19930910', // generated ethereal password
-  },
+  auth: EmailAuth,
 })
 
 const mailOptions = {
-  from: '权川的个人网站', // sender address
+  from: `权川的个人网站 <${EmailAuth.user}>`, // sender address
   to: '603803799@qq.com', // list of receivers
   // bcc: SUBSCRIBE_USERS.join(','),
   subject: 'Hello world', // Subject line
@@ -22,9 +20,15 @@ const mailOptions = {
 
 module.exports = option => {
 
-  mailOptions.form = option.title
+  mailOptions.form = `${option.title} <${EmailAuth.user}>`
   mailOptions.subject = option.subject
-  mailOptions.text = option.message
+  mailOptions.text = `
+  ${option.stdout}
+  ${option.name}\n
+  ${option.repository}\n
+  ${option.branch}\n
+  ${option.message}\n
+ `
   mailOptions.bcc = option.bcc
 
   return transporter.sendMail(mailOptions,
